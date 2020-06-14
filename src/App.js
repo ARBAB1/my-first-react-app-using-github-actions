805caf18c8aec32c2679d884772d6ab1be2ccfba
SYED ARBAB ALI SHAH <syedarbabalishah@gmail.com>
2020-06-14 17:52:10 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,8 +5,20 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      Hello from Arbab
-      This is my first app
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
